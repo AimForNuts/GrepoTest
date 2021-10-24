7369e39703e5ed74148ae97bdadc833c11edd6db
AimForNuts <josepsloliveira@gmail.com>
2021-10-25 00:59:12 +0100

    Created main file

---
+++ TamperMonkey.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name			GrepoTest
+// @namespace		GrepoTest
+// @description		Functionality settings for GrepoTest
+// @autor			AimForNuts
+// @verison			1.0
+// @include			http://*.grepolis.*/*
+// @include			https://*.grepolis.*/*
+// @updateURL 
+// ==/UserScript==
+(function () {
+    console.log("Hello world");
+})();
