8ea6631b1aa1b77eeda5afb0e477cf74b371c7e1
AimForNuts <josepsloliveira@gmail.com>
2021-10-25 01:11:17 +0100

    Added src

--- TamperMonkey.js
+++ TamperMonkey.js
@@ -6,8 +6,18 @@
 // @verison			1.0
 // @include			http://*.grepolis.*/*
 // @include			https://*.grepolis.*/*
-// @updateURL 
+// @updateURL       https://github.com/AimForNuts/GrepoTest/blob/main/TamperMonkey.js
 // ==/UserScript==
 (function () {
-    console.log("Hello world");
+    var script = document.createElement('script'),
+        link = document.createElement('link'),
+        head = document.getElementsByTagName('head')[0];
+    script.type = 'text/javascript';
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    script.src = 'https://cdn.jsdelivr.net/gh/AimForNuts/GrepoTest@main/automation.js';
+    link.href = 'https://cdn.jsdelivr.net/gh/Autarch-s/GrepoBot@master/Autobot.css';
+    head.appendChild(script);
+    head.appendChild(link);
+    head.setAttribute('xhttps', 1);
 })();
